0b3be931314d71740e8de492b66ee9c1d7a8ea3b
Jerry Prawiharjo <jprawiharjo@users.noreply.github.com>
2020-08-15 11:27:12 -0700

    Add time field. Fix time fetch

--- code.py
+++ code.py
@@ -9,10 +9,11 @@ except ImportError:
     raise
 
 # Set up where we'll be fetching data from
-DATA_SOURCE = secrets['nightscout_url']
+DATA_SOURCE = secrets["data_source"]
 BG_VALUE = [0, 'sgv']
 BG_DIRECTION = [0, 'direction']
 DATA_AGE = [0, 'date'] # This is in GMT time
+
 
 # Alert Colors
 RED = 0xFF0000;     # CRIT HIGH, CRIT LOW
@@ -31,27 +32,17 @@ def stale_data(timestamp):
 
     # stale results is the age at which results are no longer considered valid.
     # This is in minutes
-    stale_time = 6
+    stale_time = 10
 
     # Get the current timestamp in GMT
     epoch_time = time.time()
     print("Epoch GMT time:", epoch_time)
 
-    current_time_str = str(timestamp)
-
-    # nightscout sends a higher percision then is necessary and does not use dot notation
-    # so we need to cut the last three 0's off the end
-    current_time_str = current_time_str[:-3] 
-    current_time_int = int(current_time_str)
-
     # The number of minutes ago that the data was last checked
-    last_check = (epoch_time - current_time_int) /60
+    last_check = (epoch_time - int(timestamp/1000)) /60
     print("Data age: ", last_check)
 
-    if last_check > stale_time:
-        return True
-    else:
-        return False
+    return last_check > stale_time
     
 def get_bg_color(val, timestamp):
     # If the data is stale then we don't want to rely on it as an alert mech but we do need
@@ -73,6 +64,8 @@ def text_transform_bg(val):
     return str(val) + ' mg/dl'
 
 def text_transform_direction(val):
+    if val == "NONE":
+        return "↔"
     if val == "Flat":
         return "→"
     if val == "SingleUp":
@@ -80,7 +73,7 @@ def text_transform_direction(val):
     if val == "DoubleUp":
         return "↑↑"
     if val == "DoubleDown":
-        return "↓↓"
+        return "↑↑"
     if val == "SingleDown":
         return "↓"
     if val == "FortyFiveDown":
@@ -88,6 +81,10 @@ def text_transform_direction(val):
     if val == "FortyFiveUp":
         return "→↑"
     return val
+    
+def data_age(val):
+    last_check = (time.time() -  int(val/1000)) /60
+    return f"{last_check} minutes ago"
 
 # the current working directory (where this file is)
 cwd = ("/"+__file__).rsplit('/', 1)[0]
@@ -97,27 +94,29 @@ display_font = cwd+"/fonts/Arial-Bold-24-Complete.bdf"
 
 # create the display
 pyportal = PyPortal(url=DATA_SOURCE,
-                    caption_text=secrets['human'], # Name of the person being monitored
-                    caption_position=(100, 80), # This is going to be subjective to the length of the name
+                    caption_text=secrets["name"], # Name of the person being monitored
+                    caption_position=(50, 60), # This is going to be subjective to the length of the name
                     caption_font=display_font,
                     caption_color=0x000000,
                     json_path=(BG_VALUE, BG_DIRECTION, DATA_AGE),
                     status_neopixel=board.NEOPIXEL,
                     default_bg=0xFFFFFF,
                     text_font=display_font,
-                    text_position=((90, 120),   # BG location
-                                   (140, 160)), # Direction location
+                    text_position=((50, 100),   # BG location
+                                   (80, 140),
+                                   (50, 180)), # Direction location
                     text_color=(0x000000,  # BG text color
+                                0x000000,
                                 0x000000), # Direction text color
                     text_wrap=(35, # characters to wrap for BG
+                               0,
                                0), # no wrap for direction
-                    text_maxlen=(180, 30), # max text size for BG & direction
-                    text_transform=(text_transform_bg,text_transform_direction), # pre-processing of the text to ensure proper format
+                    text_maxlen=(180, 30, 100), # max text size for BG & direction
+                    text_transform=(text_transform_bg,text_transform_direction,data_age), # pre-processing of the text to ensure proper format
                    )
-
 # Preload the font for performance
 pyportal.preload_font(b'mg/dl012345789');
-pyportal.preload_font((0x2191, 0x2192, 0x2193))
+pyportal.preload_font((0x2191, 0x2192, 0x2193, 0x2197, 0x2198, 0x21d1, 0x21d3))
 
 #
 # main loop
@@ -126,7 +125,7 @@ while True:
     try:
         value = pyportal.fetch()
         print("Getting time from internet!")
-        pyportal.get_local_time(location="Africa/Abidjan")
+        pyportal.get_local_time(location=secrets["timezone"])
         pyportal.set_background(get_bg_color(value[0], value[2]))
         print("Response is", value)
 
